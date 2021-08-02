08aa73d4ae5056894bf3442c9f137e79a98fc8b0
vikusja96 <vikusja96@gmail.com>
2021-08-02 13:09:00 +0100

    Create tables

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,42 @@
-const seed = (data) => {
-  const { articleData, commentData, topicData, userData } = data;
-  // 1. create tables
+const db = require('../data');
+const format = require('pg-format');
+
+const seed = async ({ articleData, commentData, topicData, userData }) => {
+  await db.query(`DROP TABLE IF EXISTS comments`);
+  await db.query(`DROP TABLE IF EXISTS articles`);
+  await db.query(`DROP TABLE IF EXISTS users`);
+  await db.query(`DROP TABLE IF EXISTS topics`);
+
+  await db.query(`CREATE TABLE topics (
+    slug TEXT PRIMARY KEY NOT NULL,
+    description TEXT NOT NULL
+  );`);
+
+  await db.query(`CREATE TABLE users (
+    username VARCHAR(30) PRIMARY KEY NOT NULL,
+    avatar_url TEXT,
+    name VARCHAR(60) NOT NULL
+  );`);
+
+  await db.query(`CREATE TABLE articles (
+    article_id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    body TEXT NOT NULL,
+    votes INT,
+    topic TEXT REFERENCES topics(slug),
+    author VARCHAR(60) REFERENCES users(username),
+    created_at TIMESTAMP
+  );`)
+
+  await db.query(`CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    author VARCHAR(60) REFERENCES users(username),
+    article_id INT REFERENCES articles(article_id),
+    votes INT,
+    created_at TIMESTAMP,
+    body TEXT
+  );`)
+  
   // 2. insert data
 };
 
