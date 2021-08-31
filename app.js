feeda67876520411a4f2859e52ba6a6696e30a6a
vikusja96 <vikusja96@gmail.com>
2021-08-31 11:06:46 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routers/api.router");
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", apiRouter);
 app.all('*', (req, res, next) => {
